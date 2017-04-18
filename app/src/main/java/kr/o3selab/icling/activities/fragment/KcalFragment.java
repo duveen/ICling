b155163ae3aed3ca531725605b7ce3dabb721762
Duveen <duveen@duveen.me>
2017-04-18 15:54:22 +0900

    메인페이지 -> 칼로리 구현 중

--- app/src/main/java/kr/o3selab/icling/activities/fragment/KcalFragment.java
+++ app/src/main/java/kr/o3selab/icling/activities/fragment/KcalFragment.java
@@ -1,5 +1,6 @@
 package kr.o3selab.icling.activities.fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,11 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import kr.o3selab.icling.R;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
 
-/**
- * Created by O3SE Lab on 2017-04-18.
- */
+import java.util.ArrayList;
+
+import kr.o3selab.icling.R;
 
 public class KcalFragment extends Fragment {
 
@@ -23,6 +27,39 @@ public class KcalFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_kcal, null);
 
+        PieChart pieChart = (PieChart) view.findViewById(R.id.main_page_kcal_piechart);
+
+        pieChart.setData(setData());
+        pieChart.setUsePercentValues(false);
+        pieChart.getDescription().setEnabled(false);
+        pieChart.setDrawCenterText(false);
+        pieChart.setDrawHoleEnabled(true);
+        pieChart.setRotationEnabled(false);
+
+        pieChart.setHoleRadius(95.0f);
+        pieChart.setHoleColor(Color.TRANSPARENT);
+
+        pieChart.invalidate();
+
         return view;
+    }
+
+    private PieData setData() {
+        ArrayList<PieEntry> entries = new ArrayList<>();
+        entries.add(new PieEntry(0.2f));
+        entries.add(new PieEntry(0.8f));
+
+        PieDataSet dataSet = new PieDataSet(entries, "kcal");
+        dataSet.setDrawIcons(false);
+
+        ArrayList<Integer> colors = new ArrayList<>();
+        colors.add(getResources().getColor(R.color.colorPrimary));
+        colors.add(getResources().getColor(R.color.colorPrimaryDark));
+
+        dataSet.setColors(colors);
+
+        PieData data = new PieData(dataSet);
+
+        return data;
     }
 }
